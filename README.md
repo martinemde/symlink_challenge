922f1cec7355fd7269b08d1edc9018747e1d80fb
Martin Emde <martin.emde@gmail.com>
2023-02-06 16:55:14 -0800

    Make an annoying symlink filled gem

---
+++ README.md
@@ -0,0 +1,4 @@
+# SymlinkChallenge
+
+The purpose of this gem is to test various symlinks that could exist in a gem and ensure proper parsing.
+
---
+++ lib/symlink_challenge.rb
@@ -0,0 +1,6 @@
+require "symlink_challenge/version"
+
+module SymlinkChallenge
+  class Error < StandardError; end
+  # Your code goes here...
+end
